ed07ef23db8f2ce4e1c96670a4466ac6c77afc9b
U8336Tea <20009895+U8336Tea@users.noreply.github.com>
2018-06-15 22:22:37 -0700

    Added documentation because that's what a Good Programmer™ does

--- src/main/kotlin/shared/Config.kt
+++ src/main/kotlin/shared/Config.kt
@@ -1,6 +1,15 @@
 package shared
 
+/**
+ * Configuration settings for the program
+ */
 object Config {
+	/**
+	 * Get a setting
+	 *
+	 * @param key The key for the setting
+	 * @return The value for the key
+	 */
 	operator fun get(key: String): Any? {
 		return System.getenv(key)
 	}
