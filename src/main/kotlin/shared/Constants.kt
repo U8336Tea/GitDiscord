0e709abb2dc95109c1c3e9ce27f569929429c325
U8336Tea <none>
2018-06-15 22:22:37 -0700

    Added documentation because that's what a Good Programmer™ does

--- src/main/kotlin/shared/Constants.kt
+++ src/main/kotlin/shared/Constants.kt
@@ -1,4 +1,11 @@
 package shared
 
+/**
+ * The key in Config to get the GitHub auth key.
+ */
 val KEY_GITHUB_KEY = "github_key"
+
+/**
+ * The key in Config to get the Discord auth token.
+ */
 val KEY_DISCORD_TOKEN = "discord_token"
